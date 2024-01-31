48d96928fb30b4205b81930310ed70e23b10ab8f
HemanthG <hemanthgopalasetti@techvedhas.com>
2024-01-31 18:53:50 +0530

    commenting out extras

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import Use from './Use'
-import Students from './Students'
+import Students from './students'
 
 function App() {
   const [name,setName] = useState("Gojo")
@@ -9,9 +9,9 @@ function App() {
       {/* <h2>Hello</h2> */}
       <h3>There are two functions Doing their tasks in changing the outcome</h3>
       <Use/>
-      <Students name = {name} email = "thestrongest@youaretoo.weak" other= {{addr: 'Sibuya', killcount: 5000}}/>
+      {/* <Students name = {name} email = "thestrongest@youaretoo.weak" other= {{addr: 'Sibuya', killcount: 5000}}/>
       <Students name = "Itachi" email = "mangekyou@clan.killer" other= {{addr: 'Leaf village', killcount: 7000}}/>
-      <button onClick={() => {setName("Sukuna")}}>Update Me</button>
+      <button onClick={() => {setName("Sukuna")}}>Update Me</button> */}
     </div>
   )
 }
